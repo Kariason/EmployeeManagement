681b4a03253ad336264259e2a876f3347ceedeb9
Kariason <147307493+Kariason@users.noreply.github.com>
2024-08-18 23:34:09 +0500

    Добавьте файлы проекта.

---
+++ EmployeeManagement/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using EmployeeManagement.Repositories;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        private EmployeeRepository _employeeRepository { get; set; }
+        public EmployeesViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get
+            {
+                return new ObservableCollection<Employee>
+                    (_employeeRepository.GetAll());
+            }
+        }
+
+
+    }
+}
